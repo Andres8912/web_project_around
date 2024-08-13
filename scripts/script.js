6269dae7a908f7fbda8170b7953a7444597b2ae7
andresrojas <georgewter8912@gmail.com>
2024-08-12 20:50:05 -0500

    add y delete cards

--- scripts/script.js
+++ scripts/script.js
@@ -9,8 +9,39 @@ let textprofession = document.querySelector(".profile__profession");
 const btnPhoto = document.querySelector(".profile__addbutton");
 const popUpPhoto = document.querySelector("#popup-addphoto");
 const btnclosePhoto = document.querySelector("#close-photo-form");
+const templateCard = document.querySelector('.template-card');
+const cardArea = document.querySelector(".elements");
+const inputCardTitle = document.querySelector("#input-title");
+const inputCardLink = document.querySelector("#input-url");
+const formCard = document.querySelector("#photo-form");
 
 
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg"
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg"
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg"
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg"
+  }
+];
 
 
 
@@ -24,8 +55,32 @@ function handleOpenPhotoForm (){
   
 }
 
+function PhotoCreator(title, link){
+  const photo = templateCard.cloneNode(true).content.querySelector('.element');
+  const imageClone = photo.querySelector(".element__image");
+  const titleClone = photo.querySelector(".element__text");
+  const likePhoto = photo.querySelector(".element__group");
+  const deletePhoto = photo.querySelector(".element__trash");
+  imageClone.src = link;
+  titleClone.textContent = title;
+  deletePhoto.addEventListener("click", function(){
+  photo.remove();
+  })
+  likePhoto.addEventListener("click", function(){
+    likePhoto.classList.toggle("element__group-like");
+  })
+  return photo;
+}
 
-document.querySelector(".close-btn").addEventListener("click", function () {
+initialCards.forEach(function(element){
+const newPhoto = PhotoCreator(element.name, element.link);
+cardArea.append(newPhoto);
+})
+
+
+PhotoCreator();
+
+document.querySelector("#profile-close-button").addEventListener("click", function () {
   document.getElementById("popup-profile").classList.remove("popup_show");
 });
 
@@ -33,14 +88,13 @@ btnProfile.addEventListener("click", function () {
   document.getElementById("popup-profile").classList.add("popup_show");
 });
 
-document.querySelector(".close-btn").addEventListener("click",function(){
+document.querySelector("#photo-close-button").addEventListener("click",function(){
   document.getElementById("popup-addphoto").classList.remove("popup_show");
 });
 
 btnPhoto.addEventListener("click",function(){
   document.getElementById("popup-addphoto").classList.add("popup_show");
 });
-
 
 
 
@@ -53,7 +107,15 @@ textname.textContent = newName;
 textprofession.textContent = newJob;
 }
 
+function handlePhotoFormSubmit(evt){
+  evt.preventDefault();
+  const newPhoto = PhotoCreator(inputCardTitle.value, inputCardLink.value);
+  cardArea.prepend(newPhoto)
+  document.getElementById("popup-addphoto").classList.remove("popup_show");
+}
+
 profileForm.addEventListener("submit", handleProfileFormSubmit);
+formCard.addEventListener("submit", handlePhotoFormSubmit);
 
 profileForm.addEventListener('submit', function(event) {
   event.preventDefault();
